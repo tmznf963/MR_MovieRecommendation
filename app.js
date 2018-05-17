01e9943caa19ef0876e16a39a9aecd545452e0da
tmznf963 <qhfhd963@naver.com>
2018-05-17 20:38:15 +0900

    update

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var MySQLStore = require('express-mysql-session')(session);
+
+
 
 //public = 폴더 이름
 app.use(express.static('public'));
 
+//post방식 하려면 bodyParser 필요
+app.use(bodyParser.urlencoded({ extended:false}));
+
 //홈
 app.get('/',function(req, res){
-  res.send('Hello home page');
+  res.send('Hello home page');//화면출력
 });
 
 app.get('/login',function(req, res){
